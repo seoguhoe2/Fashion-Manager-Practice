79f049c8a53d97fa6aebe858324f575a2de01ef1
seoguhoe2 <js8337349@naver.com>
2025-10-14 17:53:07 +0900

    WebConfig 통합,Park 통합

--- fashion-manager2/src/main/java/fashionmanager/park/develop/menu/repository/BadgeRepository.java
+++
@@ -1,7 +0,0 @@
-package fashionmanager.park.develop.menu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import fashionmanager.park.develop.menu.Entity.Badge;
-
-
-public interface BadgeRepository extends JpaRepository<Badge, Integer> {}
