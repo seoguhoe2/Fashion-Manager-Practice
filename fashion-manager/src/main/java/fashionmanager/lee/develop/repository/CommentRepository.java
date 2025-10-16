9c95de00046cde0528d29429adc40580e52fd0b4
seoguhoe2 <js8337349@naver.com>
2025-10-16 12:31:36 +0900

    member-service msa 구현 시도중

--- fashion-manager/src/main/java/fashionmanager/lee/develop/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package fashionmanager.lee.develop.repository;
-
-import fashionmanager.lee.develop.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
